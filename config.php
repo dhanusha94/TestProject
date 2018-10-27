f571505b5805d1196880bceb230c73216c333176
dhanusha94 <43129895+dhanusha94@users.noreply.github.com>
2018-10-27 15:46:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
  
 $databaseHost = 'db4free.net';
-$databaseName = 'askdatabase';
-$databaseUsername = 'askusername';
-$databasePassword = 'askpassword';
+$databaseName = 'database';
+$databaseUsername = 'username';
+$databasePassword = 'password';
 $databasePort = '3306';
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName,$databasePort) or die("Unable to Connect to DB"); 
