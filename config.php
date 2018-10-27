4ef766f5bd58d035a37ef495960c05c6e7f23425
dhanusha94 <43129895+dhanusha94@users.noreply.github.com>
2018-10-27 14:44:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ $databaseName = 'askdatabase';
 $databaseUsername = 'askusername';
 $databasePassword = 'askpassword';
  
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName) or die("Unable to Connect to DB"); 
 ?>
