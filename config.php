17051744440ccbe10d60804969a909e2c2e8990f
dhanusha94 <43129895+dhanusha94@users.noreply.github.com>
2018-10-27 14:39:26 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
  
-$databaseHost = 'localhost';
-$databaseName = 'shangout';
-$databaseUsername = 'root';
-$databasePassword = '@connectme';
+$databaseHost = 'db4free.net';
+$databaseName = 'askdatabase';
+$databaseUsername = 'askusername';
+$databasePassword = 'askpassword';
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 ?>
