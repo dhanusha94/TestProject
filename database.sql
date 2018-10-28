e44039878bfc7ee4b8058fcbee313c81862e7dc9
dhanusha94 <43129895+dhanusha94@users.noreply.github.com>
2018-10-28 21:34:19 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,13 +1,13 @@
-create database test;
+create database database1;
 
-use test;
+use database1;
 
 CREATE TABLE `ideas` (
-  `id` int(11) NOT NULL auto_increment,
-  `description` varchar(100) NOT NULL,
-  `impact` varchar(100) NOT NULL,
-  `successmetrics` varchar(100) NOT NULL,
-  `poster` varchar(100) NOT NULL,
-  `creationtime` varchar(100) NOT NULL,
+  `id` int(32) NOT NULL auto_increment,
+  `description` varchar(1000) NOT NULL,
+  `impact` varchar(1000) NOT NULL,
+  `successmetrics` varchar(1000) NOT NULL,
+  `poster` varchar(1000) NOT NULL,
+  `creationtime` varchar(1000) NOT NULL,
   PRIMARY KEY  (`id`)
 );
